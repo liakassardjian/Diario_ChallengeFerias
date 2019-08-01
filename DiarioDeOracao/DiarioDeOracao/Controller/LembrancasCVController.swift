87782f5682be35cc8aa947df7d29caf90c49d463
liakassardjian <liakassardjian@gmail.com>
2019-08-01 16:03:23 -0300

    Adicionada fuñção de exclusão

--- DiarioDeOracao/DiarioDeOracao/Controller/LembrancasCVController.swift
+++ DiarioDeOracao/DiarioDeOracao/Controller/LembrancasCVController.swift
@@ -15,12 +15,13 @@ class LembrancasCVController: UICollectionViewController, UICollectionViewDelega
     
     @IBOutlet weak var collectionLayout: UICollectionViewFlowLayout!
     
-    @IBOutlet weak var deletarButton: UIBarButtonItem!
-    
     var context:NSManagedObjectContext?
     
     var lembrancas:[[Lembranca]] = []
     var anos:[Int] = []
+    
+    var lembrancaFoiDeletada:Bool = false
+    var lembrancaDeletada:Lembranca?
     
     var semLembrancaLabel:UILabel?
 
@@ -49,19 +50,30 @@ class LembrancasCVController: UICollectionViewController, UICollectionViewDelega
         if let label = semLembrancaLabel {
             self.view.addSubview(label)
         }
-        
-        navigationItem.leftBarButtonItem = editButtonItem
     }
     
     override func viewWillAppear(_ animated: Bool) {
         carregaLembrancas()
         collectionView.reloadData()
+
+        if lembrancaFoiDeletada {
+            if let lembranca = lembrancaDeletada {
+                let ano = Calendario.shared.retornaAno(date: lembrancaDeletada?.data?.data as! Date)
+                self.context?.delete(lembranca)
+                (UIApplication.shared.delegate as! AppDelegate).saveContext()
+                
+                carregaLembrancas()
+                collectionView.reloadData()
+            }
+            lembrancaFoiDeletada.toggle()
+        }
         
         if lembrancas.count == 0 {
             semLembrancaLabel?.isHidden = false
         } else {
             semLembrancaLabel?.isHidden = true
         }
+        
         
         
     }
@@ -85,14 +97,16 @@ class LembrancasCVController: UICollectionViewController, UICollectionViewDelega
                 lem = try context.fetch(Lembranca.fetchRequest())
             }
             
+            var a:[Int] = []
             for l in lem {
                 let data = l.data?.data as! Date
                 let ano = Calendario.shared.retornaAno(date: data)
-                if !anos.contains(ano) {
-                    anos.append(ano)
+                if !a.contains(ano) {
+                    a.append(ano)
                 }
             }
-            anos.sort(by: >)
+            a.sort(by: >)
+            anos = a
             
             lembrancas = []
             for _ in anos {
@@ -186,30 +200,6 @@ class LembrancasCVController: UICollectionViewController, UICollectionViewDelega
         }
     }
     
-    override func setEditing(_ editing: Bool, animated: Bool) {
-        super.setEditing(editing, animated: animated)
-        
-        collectionView.allowsMultipleSelection = editing
-        let indexPaths = collectionView.indexPathsForVisibleItems
-        for indexPath in indexPaths {
-            let cell = collectionView.cellForItem(at: indexPath) as! LembrancaCVCell
-            cell.isInEditingMode = editing
-        }
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        if !isEditing {
-            deletarButton.isEnabled = false
-        } else {
-            deletarButton.isEnabled = true
-        }
-    }
-    
-    override func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
-        if let selectedItems = collectionView.indexPathsForSelectedItems, selectedItems.count == 0 {
-            deletarButton.isEnabled = false
-        }
-    }
     
     // Navegação
     
@@ -221,7 +211,7 @@ class LembrancasCVController: UICollectionViewController, UICollectionViewDelega
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if let lembranca = segue.destination as? NovaLembrancaTVController {
+        if let lembranca = segue.destination as? NovaLembrancaVC {
             if segue.identifier == "editarLembranca" {
                 if let item = collectionView.indexPathsForSelectedItems?.first {
                     let titulo = lembrancas[item.section][item.row].titulo
@@ -229,14 +219,15 @@ class LembrancasCVController: UICollectionViewController, UICollectionViewDelega
                     lembranca.conteudo = [titulo,corpo] as! [String]
                     lembranca.novaLembranca = lembrancas[item.section][item.row]
                     lembranca.modoEdicao = true
+                    lembranca.lembrancaCVC = self
                 }
             }
         }
     }
     
     @IBAction func salvarEntrada(_ sender: UIStoryboardSegue){
-        if sender.source is NovaLembrancaTVController {
-            if let senderAdd = sender.source as? NovaLembrancaTVController {
+        if sender.source is NovaLembrancaVC {
+            if let senderAdd = sender.source as? NovaLembrancaVC {
                 if let lembranca = senderAdd.novaLembranca {
                     collectionView.reloadData()
                 }
@@ -245,34 +236,5 @@ class LembrancasCVController: UICollectionViewController, UICollectionViewDelega
     }
     
     
-    // Ações
     
-    @IBAction func deletarLembranca(_ sender: Any) {
-        if let selectedCells = collectionView.indexPathsForSelectedItems {
-            
-            for item in selectedCells {
-                self.context?.delete(self.lembrancas[item.section][item.row])
-                (UIApplication.shared.delegate as! AppDelegate).saveContext()
-                self.lembrancas[item.section].remove(at: item.row)
-                
-            }
-            
-            collectionView.deleteItems(at: selectedCells)
-            
-            for s in stride(from: collectionView.numberOfSections, to: 0, by: -1) {
-                if collectionView.numberOfItems(inSection: s-1) == 0 {
-                    self.lembrancas.remove(at: s-1)
-                    anos.remove(at: s-1)
-                    collectionView.deleteSections([s-1])
-                }
-            }
-            deletarButton.isEnabled = false
-            
-            if lembrancas.count == 0 {
-                semLembrancaLabel?.isHidden = false
-            } else {
-                semLembrancaLabel?.isHidden = true
-            }
-        }
-    }
 }
